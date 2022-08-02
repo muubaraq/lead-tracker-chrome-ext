54674f0bc7d679e4262e8859829fdd8e0edf6b13
mubaraq <paynedaze@gmail.com>
2022-08-02 13:58:04 +0100

    refactored

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems  
 }
+
+
+// listen for double clicks on the delte btn,  When clicked, clear localStorage, myLeads, and the DOM
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
+
+
+
+
+//localStorage.setItem("myLeads", "google.com")
+//console.log(localStorage)
+
+//localStorage.getItem("myLeads")
+//console.log(localStorage)
+
+//localStorage.clear("myLeads")
+//localStorage.clear()
+//console.log(localStorage)
+
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+// 1. Turn the myLeads string into an array
+//myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array
+//myLeads.push("www.lead2.com")
+// 3. Turn the array into a string again
+//myLeads = JSON.stringify(myLeads)
+// 4. Console.log the string using typeof to verify that it's a string
+//console.log(typeof myLeads)
